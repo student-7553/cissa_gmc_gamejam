d718f4cb95358f5a0cbd2bfab990e8ffff64894f
Nemo115 <43259251+Nemo115@users.noreply.github.com>
2025-03-08 22:14:20 +1100

    basic grid generation done

--- scripts/hex_cell.gd
+++ scripts/hex_cell.gd
@@ -1,22 +1,31 @@
-extends Node
-
-class_name hex_cell
-
+extends Node3D
+class_name Hex_Cell
 
 # Possible improvement this script itself handles showing the cell 
-
 # How do we handle the synergy
- 
+
+const CORNER_ANGLE: float = 60
+const CELL_WIDTH: float = 2
+
 var currentPoint = 0
-
 var basePoint = 0
+var save_pos: Vector3
 
-var nodePosition: Vector2 = Vector2.ZERO
+signal select_cell(cell: Hex_Cell)
 
-func init(_nodePosition: Vector2, _basePoint: int):
-	nodePosition = _nodePosition
-	basePoint = _basePoint
-	currentPoint = basePoint
-	
 func _ready() -> void:
+	save_pos = position
+
+func check_status(cell: Hex_Cell):
 	pass
+
+func mouse_entered() -> void:
+	select_cell.emit(self)
+
+func pop_up():
+	var tw: Tween = get_tree().create_tween()
+	tw.tween_property(self, "position:y", save_pos.y + 0.51, 0.25)
+
+func pop_down():
+	var tw: Tween = get_tree().create_tween()
+	tw.tween_property(self, "position:y", save_pos.y, 0.25)
--- scripts/hex_grid.gd
+++ scripts/hex_grid.gd
@@ -1,22 +1,44 @@
 extends Node3D
+class_name Hex_Grid
 
-@export var arrayRowSize = 10
-@export var arrayColumnSize = 10
-
-@export var cellScene: PackedScene
+@export var map_size = 20
+@export var tile_size = 2
+@export var player_manager: Player_Manager
+@export var tile_scene: PackedScene
 
 var grid: Array[Array] = []
 
 func _ready() -> void:
-	for n in arrayRowSize:
-		var newRow = []
+	generate_map()
 
-		for i in arrayColumnSize:
-			var cellNode = cellScene.instantiate()
 
-			add_child(cellNode)
-			newRow.append(cellNode)
+## --- LOGIC FOR HEX GRID GENERATION ---
 
-		grid.push_front(newRow)
+func generate_map():
+	for child in get_children():
+		remove_child(child)
+	for x in range(-map_size, map_size):
+		for y in range(-map_size, map_size):
+			if in_map(x,y):
+				add_tile(x, y)
 
-	pass
+func add_tile(x, y):
+	var new_tile: Hex_Cell = tile_scene.instantiate()
+	var offset: float = 0.0 if !(x % 2) else tile_size / 2.0
+	
+	add_child(new_tile)
+	new_tile.select_cell.connect(player_manager.select_new_cell)
+	
+	new_tile.translate(Vector3(x * tile_size, 0, y * tile_size + offset))
+
+func in_map(x,y):
+	var r = ceil(map_size / 2)
+	var cube = oddq_to_cube(Vector2(x, y))
+	if cube.x <= r and cube.x >= -r and cube.y <= r and cube.y >= -r and cube.z <= r and cube.z >= -r:
+		return true
+	return false
+
+func oddq_to_cube(hex):
+	var q = hex.x
+	var r = hex.y - (hex.x - (int(q)&1))/2
+	return Vector3(q, r, -q-r)
