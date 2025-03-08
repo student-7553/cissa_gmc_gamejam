8ffd5048dcb33bec31cac266053f0c9bba7b716c
Nemo115 <43259251+Nemo115@users.noreply.github.com>
2025-03-09 09:33:30 +1100

    setting up more logic for the hex cells

--- scripts/hex_grid.gd
+++ scripts/hex_grid.gd
@@ -1,16 +1,14 @@
 extends Node3D
 class_name Hex_Grid
 
-@export var map_size = 20
+@export var map_size = 8
 @export var tile_size = 2
-@export var player_manager: Player_Manager
 @export var tile_scene: PackedScene
 
-var grid: Array[Array] = []
+var grid: Dictionary[Vector3, Hex_Cell] = {} ## dictionary with key value pair of - (q, r, s) : HEX_CELL
 
 func _ready() -> void:
 	generate_map()
-
 
 ## --- LOGIC FOR HEX GRID GENERATION ---
 
@@ -21,13 +19,15 @@ func generate_map():
 		for y in range(-map_size, map_size):
 			if in_map(x,y):
 				add_tile(x, y)
+	print(grid)
 
 func add_tile(x, y):
 	var new_tile: Hex_Cell = tile_scene.instantiate()
 	var offset: float = 0.0 if !(x % 2) else tile_size / 2.0
 	
 	add_child(new_tile)
-	new_tile.select_cell.connect(player_manager.select_new_cell)
+	new_tile.cube_coord = oddq_to_cube(Vector2(x, y))
+	grid[new_tile.cube_coord] = new_tile
 	
 	new_tile.translate(Vector3(x * tile_size, 0, y * tile_size + offset))
 
@@ -38,7 +38,7 @@ func in_map(x,y):
 		return true
 	return false
 
-func oddq_to_cube(hex):
+func oddq_to_cube(hex: Vector2)->Vector3:
 	var q = hex.x
 	var r = hex.y - (hex.x - (int(q)&1))/2
 	return Vector3(q, r, -q-r)
