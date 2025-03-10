0e45d32cdbd710f3e701ae2b2155dbcdb8f6eeb2
fchemkct <hieu24a4@gmail.com>
2025-03-10 15:18:20 +1100

    GameWin and GameOver UIs implemented

--- scripts/ui/life_energy_bar.gd
+++ scripts/ui/life_energy_bar.gd
@@ -6,11 +6,16 @@ var lifeEnergyManager: LifeEnergyManager
 
 func updateUI() -> void:
 	value = int(lifeEnergyManager.currentLifeEnergy)
+	print(lifeEnergyManager.currentLifeEnergy)
+	print(value)
 	pass
 
 func _ready() -> void:
-	if lifeEnergyManager:
-		lifeEnergyManager = get_node("../../LifeEnergyManager")
+	
+	if !lifeEnergyManager:
+		lifeEnergyManager = get_node("../../../LifeEnergyManager")
+		print(lifeEnergyManager)
+	
 		lifeEnergyManager.update.connect(updateUI)
 		updateUI()
 	pass
