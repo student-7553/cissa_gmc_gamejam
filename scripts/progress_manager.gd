fbe61d5f097a8f60bab53e7b53a336c241ff5735
Nemo115 <43259251+Nemo115@users.noreply.github.com>
2025-03-11 02:24:05 +1100

    merge

--- scripts/progress_manager.gd
+++ scripts/progress_manager.gd
@@ -1,7 +1,7 @@
 extends Node
 
 @onready var life_energy_label: Label = $TotalLifeEnergy
-@onready var life_energy_bar: ProgressBar = $life_energy_bar
+@onready var life_energy_bar: TextureProgressBar = $AspectRatioContainer/life_energy_bar
 
 func _ready() -> void:
 	Globals.life_energy_manager.update.connect(updateUI)
@@ -10,6 +10,6 @@ func _ready() -> void:
 ## UPDATE THE PROGRESS BAR MAX ETC FROM NEW LEVEL
 func updateUI() -> void:
 	life_energy_label.text = str(Globals.life_energy_manager.currentLifeEnergy)
-	life_energy_bar.value = int(Globals.life_energy_manager.currentLifeEnergy)
+	life_energy_bar.value = Globals.life_energy_manager.currentLifeEnergy
 	life_energy_bar.max_value = Globals.life_energy_manager.life_energy_max
 	print("updated")
