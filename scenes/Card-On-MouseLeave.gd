3d5c8c6f9823d3958449c4fa6ad917ba90b7418c
kev-fbx <ktran041203@gmail.com>
2025-03-10 17:47:47 +1100

    Changed UI render layers (Z-index)

--- scenes/Card-On-MouseLeave.gd
+++ scenes/Card-On-MouseLeave.gd
@@ -1,15 +1,14 @@
 extends Control
-
 class_name CardHolder
-#slides the card down if the player's mouse leaves the card
+
+# Slides the card down if the player's mouse leaves the card
+
 @export var Card_Holder: Control
+@export var yPlus = 280
 
 var save_pos: Vector2
-
 var spawnedCurrentCard: TextureRect
 var cardManager: CardManager
-
-@export var yPlus = 200
 
 func _ready() -> void:
 	save_pos = position
