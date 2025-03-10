abfd2cc35cb563d675ae8a53edbdc89501912fa4
Bilguun <soproatlol@gmail.com>
2025-03-10 12:56:52 +1100

    feat: update the current card logic & update the total score

--- scripts/hex_cells/hex_cell.gd
+++ scripts/hex_cells/hex_cell.gd
@@ -13,20 +13,29 @@ var cube_coord: Vector3 ## Position in the hex grid (q, r, s)
 
 var isHovered: bool = false
 
+var lifeEnergyManager: LifeEnergyManager
+
 signal select_cell(cell: Hex_Cell)
+
+signal cell_score_change(scoreUpdate: int)
 
 func _ready() -> void:
 	save_pos = position
 	synergy.increase_score.connect(increase_score)
 
 func increase_score(amount: int):
+	cell_score_change.emit(amount)
 	current_score += amount
 
 ## Called when a cell is being replaced
 func copy_cell_data(cell: Hex_Cell):
 	cube_coord = cell.cube_coord
 	save_pos = cell.save_pos
+
+	cell_score_change.emit(- current_score)
 	current_score = cell.current_score
+	cell_score_change.emit(current_score)
+
 	base_score = cell.base_score
 	position = cell.position
 
